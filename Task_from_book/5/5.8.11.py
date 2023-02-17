ea8201f88db49519a7cb71cae3b8322cc0ab96f8
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2023-02-17 13:31:52 +0100

    added more task for chapter 5 and added them in folder

--- Task_from_book/5/5.8.11.py
+++ Task_from_book/5/5.8.11.py
@@ -10,26 +10,49 @@ def pow_method(p1, v0, n1):
             N:   number of iterations
         Output:
             v:   final vector of probabilities v=P^n * v0
+            my:  approaches the dominant eigenvalue
     """
     v = v0
+    my = 0
     for i in range(n1):
         v = np.matmul(p1, v)
         my = np.max(v)
         v = 1 / my * v
+
     return v, my
 
 
-P = np.array([[6, 7], [8, 5]])
+def rayleigh_quotient(a, v_f):
+    """
+    rayleigh_quotient(a, v_f)
+        Input:
+            a:   matrix
+            v_f:  initial vector of probabilities
+        Output:
+            r:   approximation to eigenvalue
+    """
+    v_t = np.transpose(v_f)
+    temp = np.matmul(a, v_f)
+    r = (np.matmul(v_t, temp))/(np.matmul(v_t, v_f))
+
+    return r
+
+
+P = np.array([[5, 2], [2, 2]])
 X0 = np.array([[1], [0]])
 k1 = 1
 k2 = 2
 k3 = 3
 k4 = 4
 k5 = 5
-k6 = 6
 pm1, my0 = pow_method(P, X0, k1)
 pm2, my1 = pow_method(P, X0, k2)
 pm3, my2 = pow_method(P, X0, k3)
 pm4, my3 = pow_method(P, X0, k4)
 pm5, my4 = pow_method(P, X0, k5)
-pm6, my5 = pow_method(P, X0, k6)
+
+r1 = rayleigh_quotient(P, pm1)
+r2 = rayleigh_quotient(P, pm2)
+r3 = rayleigh_quotient(P, pm3)
+r4 = rayleigh_quotient(P, pm4)
+r5 = rayleigh_quotient(P, pm5)
---
+++ Task_from_book/5/5.8.17.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def inverse_pow_method(p1, v0, n1, alpha):
+    """
+    inverse_pow_method(P, v0, N):
+        Input:
+            P:   transition matrix
+            v0:  initial vector of probabilities
+            N:   number of iterations
+            alpa: estimation of the eigenvalue
+        Output:
+            v_k: approximation of the eigenvalue
+            my:  approaches the dominant eigenvalue
+    """
+    v = v0
+    v_k = 0
+    my = 0
+    for i in range(n1):
+        i = np.shape(p1)
+        inverse = np.linalg.inv(p1-alpha*np.identity(i[1]))
+        y_k = np.matmul(inverse, v)
+        my = np.max(y_k)
+        v_k = alpha + 1 / my
+        v = (1 / my) * y_k
+    return v_k, my
+
+
+P = np.array([[10, -8, -4], [-8, 13, 4], [-4, 5, 4]])
+X0 = np.array([[1], [0], [0]])
+alpa = 3.3
+k1 = 1
+k2 = 2
+k3 = 3
+
+pm1, my0 = inverse_pow_method(P, X0, k1, alpa)
+pm2, my1 = inverse_pow_method(P, X0, k2, alpa)
+pm3, my2 = inverse_pow_method(P, X0, k3, alpa)
